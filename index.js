0cd86a0f1b43169474ad4c87da8a60746d9f5a7c
Noah Beidelman <nbeidelman9@gmail.com>
2023-06-19 18:34:00 -0700

    remove tempo_ms

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //tempo in bpm, between 50 - 250
 let tempo = $("#tempo-slider")[0].value;
-let tempo_ms = tempo * 60 * 1000;
+
 //vol between 0 - 100
 let vol = $("#volume-slider")[0].value;
 
@@ -11,14 +11,8 @@ function adjustVol() {
 
 function adjustTempo() {
     tempo = $("#tempo-slider")[0].value;
-    tempo_ms = tempo * 60 * 1000;
     sound.rate(tempo / 99);
-    // change the speed of the audio playback
 }
-/*
-rate Number 1.0
-The rate of playback. 0.5 to 4.0, with 1.0 being normal speed.
-*/
 
 var sound = new Howl({
     src: ['audio/fart.mp3'],
