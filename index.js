768bdce533004b3341cb7f03930bdb51856c49cc
Noah Beidelman <nbeidelman9@gmail.com>
2023-06-19 18:15:22 -0700

    make sliders work

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-//tempo in bpm
+//tempo in bpm, between 50 - 250
 let tempo = $("#tempo-slider")[0].value;
+let tempo_ms = tempo * 60 * 1000;
 //vol between 0 - 100
 let vol = $("#volume-slider")[0].value;
 
-tempo_ms = tempo * 60 * 1000;
-Howler.volume(vol * .01);
+function adjustVol() {
+    vol = vol = $("#volume-slider")[0].value;
+    Howler.volume(vol * .01);
+}
+
+function adjustTempo() {
+    tempo = $("#tempo-slider")[0].value;
+    tempo_ms = tempo * 60 * 1000;
+    sound.rate(tempo / 99);
+    // change the speed of the audio playback
+}
+/*
+rate Number 1.0
+The rate of playback. 0.5 to 4.0, with 1.0 being normal speed.
+*/
 
 var sound = new Howl({
-    src: ['fart.mp3']
+    src: ['fart.mp3'],
+    loop: true
 });
 
-$(document).on("click", () => {
-    sound.play();
+$("#play").on("click", () => {
+    let sound1 = sound.play();
 })
+
+$("#volume-slider").change(adjustVol)
+$("#tempo-slider").change(adjustTempo)
